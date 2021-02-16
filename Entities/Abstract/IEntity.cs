0d69c14d5755e5a21afd7b16590a001a5c8b18bd
Güldeniz Özdemir <78044804+Deniz000@users.noreply.github.com>
2021-02-16 15:54:25 +0300

    first commit

---
+++ Entities/Abstract/IEntity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Abstract
+{
+    public interface IEntity
+    {
+        //interface oluşturmak işi sağlama almaktır. 
+        // ve de ıEntity alt sınıfların referansını tutabiliyor.
+
+    }
+}
---
+++ Entities/Concrete/Car.cs
@@ -0,0 +1,19 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Concrete
+{
+    public class Car:IEntity //default erişim bildirgeci internal
+    {
+        public int Id { get; set; }
+        public int BrandId { get; set; }
+        public int ColorId { get; set; }
+        public int ModelYear { get; set; }
+        public int DailyPrice { get; set; }
+        public string Description { get; set; }
+            
+
+    }
+}
